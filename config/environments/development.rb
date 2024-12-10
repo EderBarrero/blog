f976e6446515ae8b5cca1b6a3dd08d0d38af9d10
EderBarrero <ebarrero@abatechnology.com>
2024-12-10 10:19:25 -0500

    credentials well

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
+    domain:               nil,
     user_name:            Rails.application.credentials.gmail[:user_name],
     password:             Rails.application.credentials.gmail[:password],
     authentication:       'plain',
