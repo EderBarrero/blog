b12956a66f56716d271d57255b0823be2ec39102
EderBarrero <ebarrero@abatechnology.com>
2024-12-10 11:11:53 -0500

    Ome

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "BlogWithoutGoogle_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: 'jbc3163@gmail.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               nil,
+    user_name:            Rails.application.credentials.gmail[:user_name],
+    password:             Rails.application.credentials.gmail[:password],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   config.action_mailer.default_url_options = { host: 'mysite-r99r.onrender.com', protocol: 'https' }
 
