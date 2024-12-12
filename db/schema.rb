c6b4a151c39f090f887a8902318e6a578dc6cbbe
EderBarrero <ebarrero@abatechnology.com>
2024-12-12 15:47:52 -0500

    add independent views for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_09_014304) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_12_174258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,7 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_09_014304) do
     t.integer "role", default: 0
     t.string "first_name"
     t.string "last_name"
-    t.integer "status"
+    t.integer "status", default: 1
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
